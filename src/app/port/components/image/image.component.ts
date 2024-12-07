f996690ba271104b831c3e66a307fd9d63dab655
Rhidalgo12 <raul.hidalgo@alumnos.ucn.cl>
2024-12-07 10:35:09 -0300

    feat: add image and description and rol

---
+++ src/app/port/components/image/image.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'port-image',
+  standalone : true,
+  imports: [CommonModule],
+  templateUrl: './image.component.html',
+  styleUrl: './image.component.css'
+})
+export class ImageComponent {
+
+}
--- src/app/port/pages/port-home/port-home.component.ts
+++ src/app/port/pages/port-home/port-home.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ImageComponent } from '../../components/image/image.component';
 
 @Component({
   selector: 'app-port-home',
-  imports: [CommonModule],
+  imports: [CommonModule, ImageComponent],
   templateUrl: './port-home.component.html',
   styleUrl: './port-home.component.css'
 })
