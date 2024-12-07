09d6d4be29c03c699ea0116fa67859602d52dda7
Rhidalgo12 <raul.hidalgo@alumnos.ucn.cl>
2024-12-06 23:17:20 -0300

    feat: add pages and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        loadComponent: () => import('./port/pages/port-home/port-home.component').then((m) => m.PortHomeComponent)
+    },
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    },
+    {
+
+        path: '**',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    }
+];
---
+++ src/app/port/pages/port-home/port-home.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-port-home',
+  imports: [CommonModule],
+  templateUrl: './port-home.component.html',
+  styleUrl: './port-home.component.css'
+})
+export class PortHomeComponent {
+
+}
