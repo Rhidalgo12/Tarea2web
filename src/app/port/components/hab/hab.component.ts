a2a31d5b5b260d6f6d920f8fa376c8230bafa5a2
Rhidalgo12 <raul.hidalgo@alumnos.ucn.cl>
2024-12-07 10:58:26 -0300

    feat: add skills

---
+++ src/app/port/components/hab/hab.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'port-hab',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './hab.component.html',
+  styleUrl: './hab.component.css'
+})
+export class HabComponent {
+
+}
--- src/app/port/pages/port-home/port-home.component.ts
+++ src/app/port/pages/port-home/port-home.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ImageComponent } from '../../components/image/image.component';
+import { HabComponent } from '../../components/hab/hab.component';
 
 @Component({
   selector: 'app-port-home',
-  imports: [CommonModule, ImageComponent],
+  imports: [CommonModule, ImageComponent, HabComponent],
   templateUrl: './port-home.component.html',
   styleUrl: './port-home.component.css'
 })
